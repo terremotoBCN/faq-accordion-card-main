02f087d95f159f28c87fb969827e641480cde9e7
terremotoBCN <terremoto.news@gmail.com>
2021-10-23 14:38:11 +0200

    Mobile listo - Nueva animacion desplegar - Fine tunes

--- js/behavior.js
+++ js/behavior.js
@@ -2,32 +2,47 @@
 
 const faqArticles = document.getElementsByClassName("faq__article");
 
-// console.log(faqArticles);
-
-function clearActives() {
-
-    for (const element of faqArticles) {
-        element.classList.remove("active");
-    }
-
-}
-
-
-function setActive (event) {
-    const selArticle = event.currentTarget;
-    let isActive = selArticle.classList.contains("active");
-    clearActives();
-    // console.log(isActive);
-    if (isActive) {
-        selArticle.classList.remove("active");
-    } else {
-        selArticle.classList.add("active");
-    }
-
-}
 
 for (var a = 0; a < faqArticles.length; a++) {
 
     faqArticles[a].addEventListener("click", setActive, false);
 
+}
+
+
+async function clearActives() {
+    const elementsActives = document.getElementsByClassName("active");
+
+    for (let item of elementsActives) {
+
+        item.classList.remove("active");
+        // console.log(`remove active options from ${item.id} element`);
+
+        const itemText = item.getElementsByTagName("p");
+        // console.log(`elementos encontrados ${itemText.length}`);
+        for (let item of itemText) {
+            item.removeAttribute("style");
+
+        }
+
+    }
+
+
+}
+
+
+function setActive(event) {
+    const selArticle = event.currentTarget;
+
+    const textArticle = selArticle.getElementsByClassName("faq__article-text");
+    // const textArticle2 = event.currentTarget.getElementsByClassName("faq__article-text");
+
+    const isActive = selArticle.classList.contains("active");
+
+    clearActives();
+
+    if (!isActive) {
+        selArticle.classList.add("active");
+        textArticle[0].style.height = `${textArticle[0].scrollHeight}px`;
+    }
 }
